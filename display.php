3f9aa63b4a8b1c1ff43dcb8ec1e845a0ade7e8bc
Tanmay Bangale <bangaletanmay@gmail.com>
2017-09-08 21:17:15 +0530

    Update display.php

--- display.php
+++ display.php
@@ -5,7 +5,16 @@
 		  
 		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		   <link  href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
 
+		   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+		  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>       
 
 		</head>
 
